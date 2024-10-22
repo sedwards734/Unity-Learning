38b0c79d56f674fdc096c16f5368052892dccb30
Sam Edwards <sedward@iastate.edu>
2024-10-22 09:56:03 -0500

    More Roll A Ball

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -8,34 +8,46 @@ using TMPro;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+
     private int count;
+
     private float movementX;
     private float movementY;
+
     public float speed = 0;
+
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
+
+    private GameObject[] enemyArray = new GameObject[2];
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
         count = 0;
+
         SetCountText();
+
         winTextObject.SetActive(false);
-    }
-    private void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
     }
 
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
-    private void OnTriggerEnter(Collider other)
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pickup"))
         {
@@ -50,9 +62,29 @@ public class PlayerController : MonoBehaviour
     {
         countText.text = "Count: " + count.ToString();
 
-        if (count >= 12)
+        if (count >= 9)
         {
             winTextObject.SetActive(true);
+            // Make a new gameObject that stores an array
+            enemyArray = (GameObject.FindGameObjectsWithTag("Enemy"));
+
+            for (int i = 0; i < enemyArray.Length; i++)
+            {
+                Destroy(enemyArray[i]);
+            }
+            // Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+            
+        }  
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
         }
     }
 }
